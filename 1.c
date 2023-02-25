ab1295f7aa60b999fdb262e8b005db4659b9e2e2
ChandragiriRishitha <120772607+ChandragiriRishitha@users.noreply.github.com>
2023-02-25 14:29:40 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,5 +1,6 @@
-#include<stdio.h>
-int main(){
-printf("hello world");
-return 0;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
